c2113a6467b5c4bd234d5fa9112325535002fc0f
Aleksei <kav.prok@gmail.com>
2024-07-04 15:00:55 +0700

    refactor: change styles and card component

--- src/components/card/card.compomemt.tsx
+++ src/components/card/card.compomemt.tsx
@@ -10,17 +10,19 @@ export class Card extends React.Component<ICard> {
   render() {
     return (
       <div className="card__item">
-        <h2 className="card__title">{this.props.data.name}</h2>
-        <p>
-          <span className="card__span">Heigth:</span> {this.props.data.height}
-        </p>
-        <p>
-          <span className="card__span">Mass:</span> {this.props.data.mass}
-        </p>
-        <p>
-          <span className="card__span">Hair color:</span>{' '}
-          {this.props.data.hair_color}
-        </p>
+        <div className="card__content">
+          <h2 className="card__title">{this.props.data.name}</h2>
+          <p>
+            <span className="card__span">Heigth:</span> {this.props.data.height}
+          </p>
+          <p>
+            <span className="card__span">Mass:</span> {this.props.data.mass}
+          </p>
+          <p>
+            <span className="card__span">Hair color:</span>{' '}
+            {this.props.data.hair_color}
+          </p>
+        </div>
         <div className="card__backdrop">{this.props.data.name}</div>
       </div>
     );
