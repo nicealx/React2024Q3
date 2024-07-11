cb42fc24132744e7c229240ea893bb25053b3c6e
Aleksei <kav.prok@gmail.com>
2024-07-11 13:38:37 +0700

    refactor: remove context in header component

--- src/modules/header/component/header.module.tsx
+++ src/modules/header/component/header.module.tsx
@@ -1,13 +1,11 @@
-import { FC, useContext } from 'react';
+import { FC } from 'react';
 import { Search } from '../../../components';
-import { AppContext } from '../../../store/AppContext';
 import './header.css';
 
 export const Header: FC = () => {
-  const { isLoading, search } = useContext(AppContext);
   return (
     <header className="header">
-      <Search isLoading={isLoading} search={search} />
+      <Search />
     </header>
   );
 };
